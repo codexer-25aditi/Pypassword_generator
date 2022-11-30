f380c0f1b47051cca6f030e8e458d76ef14641ff
ADITI SAHU <88385979+codexer-25aditi@users.noreply.github.com>
2022-11-30 12:42:05 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+# Go to: https://replit.com/@appbrewery/password-generator-start?v=1
+# Password Generator Project
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+# Eazy Level - Order not randomised:
+# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+pwd = ""
+for i in range(0, nr_letters):
+    l = random.randint(0, len(letters) - 1)
+    pwd = pwd + letters[l]
+for i in range(0, nr_symbols):
+    n = random.randint(0, len(symbols) - 1)
+    pwd = pwd + symbols[n]
+for i in range(0, nr_numbers):
+    num = random.randint(0, len(numbers) - 1)
+    pwd = pwd + numbers[n]
+print("your password is generated:", pwd)
+p = list()
+for i in pwd:
+    p.append(i)
+random.shuffle(p)
+delimiter=""
+a=delimiter.join(p)
+print("your strong password generated:", a)
+while (True):
+    ans = input("Are you satisfies press y for yes or n for no")
+    if ans == "y":
+        break
+    elif ans == "n":
+        p = random.shuffle(p)
+        print(p)
+        break
+
+
